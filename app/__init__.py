8a1edb7470f82a9745174a356d4af557d0060d87
Julian Pavlov <julian.pavlov@raftds.com>
2024-10-25 10:41:12 +0300

    add teltgram bot for analyzis access

--- app/__init__.py
+++ app/__init__.py
@@ -2,16 +2,16 @@
 
 from flask import Flask
 from config import Config
+from app.routes import main_bp
+import os
 
 def create_app(config_class=Config):
     app = Flask(__name__)
     app.config.from_object(config_class)
 
     # Ensure the upload folder exists
-    import os
     os.makedirs(app.config['UPLOAD_FOLDER'], exist_ok=True)
 
-    from app.routes import main_bp
     app.register_blueprint(main_bp)
 
     return app
